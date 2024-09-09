c3353f677583b27eb8190cdcd3399bf8e15d8926
Webis-2022 <verjovkin.jevgeni@gmail.com>
2024-09-09 22:57:00 +0300

    Task is done

--- src/index.js
+++ src/index.js
@@ -35,11 +35,57 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    console.log(expr);
+    let substr;
+    let subArr = [];
+    let length = expr.length / 10;
+  
+    for (let i = 0; i < length; i++) {
+      substr = expr.substring(0, 10);   
+      let arr = substr.split('');      
+      subArr.push(arr);                
+      expr = expr.slice(10);             
+    }
+  
+    subArr.forEach(el => {
+      let firstOneIndex = el.indexOf('1'); 
+      if (firstOneIndex !== -1) {
+          el.splice(0, firstOneIndex);     
+      }
+    });
+  
+    let result = subArr.map(subArray => {
+    let grouped = [];
+    for (let i = 0; i < subArray.length; i += 2) {
+        let pair = subArray[i] + (subArray[i + 1] || '');
+        grouped.push(pair);
+    }
+    return grouped;
+    });
+    let newArr = result.map(subArr => 
+        subArr.map(item => {
+            item = item.replace(/11/g, '-');
+            item = item.replace(/10/g, '.');
+            return item;
+        })
+    );
+  
+    let merged = newArr.map((subArr) => subArr.reduce((acc, item) => {
+        return acc + item;
+    })
+    )
+    let resArr = [];
+    for(let h = 0; h < merged.length; h++) {
+      let letter = MORSE_TABLE[merged[h]];
+      resArr.push(letter);
+    }
+    return resArr.join('');
+  }
+
 
 module.exports = {
     decode
